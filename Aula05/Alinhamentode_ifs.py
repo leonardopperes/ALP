a8e38b42341729487276335c59bf9bbbf71a7801
Leonardo Proença Peres <leonardo.peres4@fatec.sp.gov.br>
2024-03-18 22:37:34 -0300

    Aula 5

--- Aula05/Alinhamentode_ifs.py
+++ Aula05/Alinhamentode_ifs.py
@@ -2,14 +2,13 @@ a = float(input("Digite o Lado 1: "))
 b = float(input("Digite o Lado 2: "))
 c = float(input("Digite o Lado 3: "))
 
-if(((a+b)<c) or ((b+c)<a) or ((a+c)<b)):
-    print ("Isso não é um triangulo")
+if(((a+b)>c) and ((b+c)>a) and ((a+c)>b)):
+    print ("Isso é um triangulo")
+    if ((a == b) and (b == c)):
+        print("Isso é um triângulo equilatero")
+    elif ((a == b) or (a == c) or (b == c)):
+        print("Isso é um triângulo Isósceles")
+    else:
+        print("Isso é um triangulo Escaleno")
 else:
-    print("Isso é um triangulo")
-
-if((a==b) and (b==c)):
-    print("Isso é um triângulo equilatero")
-elif((a==b) or (a==c) or (b==c)):
-    print("Isso é um triângulo Isósceles")
-else:
-    print("Isso é um triangulo Escaleno")
+    print("Isso não é um triangulo")
